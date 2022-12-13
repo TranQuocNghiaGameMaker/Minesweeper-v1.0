e461c6d3f2700e4d3e929617b667a2803e369421
Trần Quốc Nghĩa 21011618 <garenpc2003@gmail.com>
2022-12-13 20:00:25 +0700

    Create GenerateMines System

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,16 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] int width = 16;
-    [SerializeField] int height = 16;
-    public int Width => width;
-    public int Height => height;
+    [SerializeField] int _width = 16;
+    [SerializeField] int _height = 16;
+    [SerializeField] int _mineCount;
+    public int Width => _width;
+    public int Height => _height;
     private Board _board;
-    private Cell[,] _state;
+    private Cell[,] _state { get; }
 
     // Start is called before the first frame update
     private void Awake()
@@ -24,21 +24,45 @@ public class GameManager : MonoBehaviour
 
     private void NewGame(Cell[,] cells)
     {
-        cells = new Cell[width,height];
+        cells = new Cell[_width,_height];
         GenerateCell(cells);
+        GenerateMines(cells);
         _board.Draw(cells);
     }
 
     private void GenerateCell(Cell[,] cells)
     {
-        for(int i = 0;i < width; i++)
+        for(int i = 0;i < _width; i++)
         {
-            for(int j = 0;j < height; j++)
+            for(int j = 0;j < _height; j++)
             {
                 var cell = new Cell();
                 cell.Position = new(i, j);
                 cells[i, j] = cell;
             }
+        }
+    }
+    private void GenerateMines(Cell[,] cells)
+    {
+        for(int i = 0;i < _mineCount; i++)
+        {
+            var x = Random.Range(0, Width);
+            var y = Random.Range(0, Height);
+            while (cells[x,y].CellType == Cell.Type.mine)
+            {
+                x++;
+                if(x >= Width)
+                {
+                    x = 0;
+                    y++;
+                    if(y >= Height)
+                    {
+                        y = 0;
+                    }
+                }
+            }
+            cells[x, y].CellType = Cell.Type.mine;
+            cells[x, y].Revealed = true;
         }
     }
 }
