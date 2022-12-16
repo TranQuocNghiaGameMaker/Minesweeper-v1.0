d6e64b9eed7337cb26e53257df1e8a92c8119f4c
Trần Quốc Nghĩa 21011618 <garenpc2003@gmail.com>
2022-12-16 19:44:28 +0700

    make neighbor cell to reveal when click on None Cell by Flooding function (Recursive)

--- Assets/Scripts/UpdateBoardSystem.cs
+++ Assets/Scripts/UpdateBoardSystem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,10 +20,31 @@ public class UpdateBoardSystem : MonoBehaviour
     {
         Cell cell = GetCell(cellPosition.x, cellPosition.y);
         if (cell.CellType == Cell.Type.invalid || cell.Revealed || cell.Flagged) return;
+        if(cell.CellType == Cell.Type.none)
+        {
+            Flood(cell);
+        }
         cell.Revealed = true;
         State[cellPosition.x, cellPosition.y] = cell;
         VisualBoardSystem.ChangeBoardAction(State);
     }
+
+    private void Flood(Cell cell)
+    {
+        if (cell.Revealed) return;
+        if (cell.CellType == Cell.Type.mine || cell.CellType == Cell.Type.invalid) return;
+
+        cell.Revealed = true;
+        State[cell.Position.x, cell.Position.y] = cell;
+        if (cell.CellType == Cell.Type.none)
+        {
+            Flood(GetCell(cell.Position.x, cell.Position.y + 1));
+            Flood(GetCell(cell.Position.x, cell.Position.y - 1));
+            Flood(GetCell(cell.Position.x - 1, cell.Position.y));
+            Flood(GetCell(cell.Position.x + 1, cell.Position.y));
+        }
+    }
+
     private Cell GetCell(int x, int y)
     {
         if (IsValid(x, y))
